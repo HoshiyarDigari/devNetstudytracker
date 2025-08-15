d4c97aad8fb77572b1686b014c722688c1ca88fa
HoshiyarDigari <hoshiyarsingh@gmail.com>
2025-08-15 19:33:51 -0400

    gcd solved

--- gcd_of_strings.py
+++ gcd_of_strings.py
@@ -1,13 +1,15 @@
-def isFactor(factor, word):
-    multiple = factor
-    count=1
-    while len(multiple)<=len(word):
-        multiple=factor*count
-        if multiple == word:
-            return True
-        else:
-            count+=1
-    return False
+def isRepeatedPattern(pattern:str, word:str) -> bool:
+    """
+    checks if the word can be constructed by repeating the pattern
+    Args:
+        pattern (str): the substring to use as a repeating unit.
+        word (str): the full string to test against the repeated pattern
+        bool: True if word can be constructed by repeating the pattern, False otherwise
+    """
+    # a valid pattern must repeat itself to construct the word
+    # the number of repetitions should make the repeated pattern match the word length.
+    return pattern * (len(word)//len(pattern))==word 
+    
 def gcd(word1, word2):
     shorter = word1 if len(word1)<= len(word2) else word2
     longer = word2 if len(word2)>len(word1) else word1
@@ -20,7 +22,7 @@ def gcd(word1, word2):
         if len(longer)%len(divisor)!=0:
             continue
 
-        if isFactor(divisor, longer) and isFactor(divisor, shorter):
+        if isRepeatedPattern(divisor, longer) and isRepeatedPattern(divisor, shorter):
             return divisor 
     return ''
 
