0448a923cfd479cc3a65abb5dd341fa633e59154
HoshiyarDigari <hoshiyarsingh@gmail.com>
2025-09-22 17:43:02 -0400

    completed the debug url checker function

--- Akamai-URL-checker/static/runtime.js
+++ Akamai-URL-checker/static/runtime.js
@@ -23,6 +23,48 @@ function changeVisibility(clickElementId, targetElementId) {
 }
 
 
+function parseRawHeaders(){
+    // capture status code
+    const statusCode = document.getElementById("status_code").textContent;
+    let redirectUrl = '';
+    let status = "FAIL";
+    if (statusCode >= 200 && statusCode <=299 ){
+        status = "Success"
+    }
+    if (statusCode >=300 && statusCode <=399){
+        status = "Redirect" ;
+        redirectUrl = document.getElementById("Location").textContent.trim();
+    }
+    let message = `The request was ${status} with HTTP status code ${statusCode}.<br>`;
+    if (status === "Redirect") {
+        message+=`The redirect location is ${redirectUrl}<br>`
+    };
+    const propertyName = document.getElementById("name=AKA_PM_PROPERTY_NAME").textContent.replace("value=", '');
+    
+    const propertyVersion = document.getElementById("name=AKA_PM_PROPERTY_VERSION").textContent.replace("value=",'');
+    let edgeIP = document.getElementById("X-Cache").textContent;
+    const ipParts = edgeIP.match(/from\s+([^-]+)-([^-]+)-([^-]+)-([^.]+)/);
+    if (ipParts) {
+        edgeIP = `${ipParts[1]}.${ipParts[2]}.${ipParts[3]}.${ipParts[4]}`; 
+    }
+    message += `The request was processed by Akamai edge IP ${edgeIP} using rules from property ${propertyName}, version ${propertyVersion}.<br>`;
+    let cache_hit = document.getElementById("X-Cache").textContent.trim();
+    // cache hit will be true if "HIT" is found in the string else it will be false
+    cache_hit = cache_hit.toLowerCase().includes("hit");
+    message+= cache_hit? "The response was served from cache.<br><br>": "The response was fetched from origin.<br>";
+    // check if there is a server header
+    const origin = document.getElementById("Server")
+    if (origin) {
+        message+=`The origin server is ${origin.textContent.trim()}.<br>`
+    }
+    // select all elements whose id begins with ALB string, which should be just one, we then format it to just get teh alb origin name, because we are looking at a cookie value
+    const alb_origin = document.querySelectorAll('[id^="ALB"]')[0].textContent.split(';',1);
+    if (alb_origin) {
+        message+=`The request was assigned an ALB origin: ${alb_origin}`;
+    }
+    document.getElementById('summary').innerHTML = `<h3>Summary</h3><p>${message}</p>`;
+}
+
 // We only invoke the JS functions once the page is fully loaded
 window.addEventListener("DOMContentLoaded", ()=>{
 
@@ -58,7 +100,9 @@ window.addEventListener("DOMContentLoaded", ()=>{
                     body: new FormData(form)
                 })
                 .then(response => {return response.text()})
-                .then(html => {document.getElementById('toolArea').innerHTML = html})
+                .then(html => {document.getElementById('toolArea').innerHTML = html;
+                    parseRawHeaders();
+                })
                 .catch(error => {console.log('error', error)});
                 
                 
@@ -70,6 +114,5 @@ window.addEventListener("DOMContentLoaded", ()=>{
             console.error("Fetch Error", error);
         })
     });
-
     
 })
